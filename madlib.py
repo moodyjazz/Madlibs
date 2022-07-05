f4fa5bf4ddbeb7c643502ba6297adbef1aa1d8f8
moodyjazz <53325901+moodyjazz@users.noreply.github.com>
2022-07-05 12:29:41 -0700

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,33 @@
+#Random madlib that I made about the League of Legends Solo Queue Experience lmao
+
+proper_noun1 = input("Proper Noun: ")
+neg_adj1 = input("Negative Adjective: ")
+body_part = input("Body Part: ")
+neg_verb1 = input("Negative Verb: ")
+adj_1 = input("Adjective: ")
+adj_2 = input("Adjective: ")
+Exclamation = input("Exclamation: ")
+role = input("Summoner Rift Role: ")
+neg_adj2 = input("Adjective: ")
+animal = input("Animal: ")
+verb_ends_with_ing = input("Verb that Ends with 'Ing': ")
+fruit1 = input("Fruit: ")
+important_historical_figure = input("Important Historical Figure: ")
+past_tense_verb = input("Past Tense Verb: ")
+extremely_neg_adj = input("Extremely Negative Adjective: ")
+emotion = input("Emotion: ")
+verb1 = input("Verb: ")
+random_proper_noun = input("Random Proper Noun: ")
+random_proper_noun2 = input("Random Noun: ")
+weather = input("Weather: ")
+verb2 = input("Verb: ")
+
+madlib = f"My name is {proper_noun1}. I love playing League of Legends even when it is so {neg_adj1}. \
+My {body_part} {neg_verb1} because of the {adj_1} amount of hours I put into this {adj_2} game. {Exclamation}! \
+This is such a {role} diff. This {neg_adj2} player literally fed our mid-lane like a {animal} that is \
+{verb_ends_with_ing} a big {fruit1}. If only {important_historical_figure} {past_tense_verb} from the heavens, \
+only they would be able to save this {extremely_neg_adj} game. I am filled with so much {emotion} that I \
+cannot {verb1} my {random_proper_noun} and now I have to shovel my {random_proper_noun2}. We should just \
+surrender before the {weather} comes and makes me {verb2} down and cry myself to sleep."
+
+print(madlib)
